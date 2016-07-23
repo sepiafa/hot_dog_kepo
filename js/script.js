aeb61a5e110e2c7bc6e5825a6d73a674d58ecf3a
dwiyemi__ <dwihandayani326@gmail.com>
2016-07-23 16:57:38 +0700

    penambahan file gallery.html

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function readMore(){
+    var temp = document.getElementById("More_Woofs").style.display;
+    if(temp=="none")
+        document.getElementById("More_Woofs").style.display="block";
+    else
+        document.getElementById("MoreWoofs").style.display="none";
+}
